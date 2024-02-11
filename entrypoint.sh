9804cbd34f629ffa59493df3f0901ae78ab4612c
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2024-02-12 01:43:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,3 @@ uname -m
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 curl -Lo ./globed-game-server-x64 https://github.com/dankmeme01/globed2/releases/download/v${VERSION}/globed-game-server-x64
 chmod +x ./globed-game-server-x64
-
-# Run the Server
-./{{GLOBED_FILE}} {{IP_ADDRESS}}:{{IP_PORT}} {{CENTRAL_ADDRESS}} {{PASSWORD}}
